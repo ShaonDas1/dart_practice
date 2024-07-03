7e9710768c73d294efcafba50df1ac33c9e41ec6
shaon <dashaon72@gmail.com>
2024-07-03 21:11:33 +0600

    Practice on dart 24

---
+++ bin/basice_practice.dart
@@ -0,0 +1,46 @@
+//Data type and variable
+
+//data type
+//String
+//integer
+//double
+//boolean
+void main(){
+  // Strings are mainly used to represent text
+  String shaon='How are you,joy';
+  String joy ='good,what about you';
+  print(shaon);
+  print(joy);
+
+  // In Dart programming language, integers are used to represent whole numbers.
+  int a=33;
+  double b=77.7;
+  print(a);
+  print(b);
+
+  //Summation on Dart
+  int sum1=20;
+  int sum2=20;
+  int summation=sum1+sum2;
+  print(summation);
+
+  //double & integer summation
+  int a1=24;
+  double b2=22.3;
+  double sum=a1+b2;
+  print(sum);
+
+  //Boolean(true or false)
+  bool isstudent=true;
+  bool chiter=false;
+  print(isstudent);
+  print(chiter);
+
+  //typ casting
+  double f1=3.44;
+  int f2=4;
+  int total =f1.toInt()+f2;
+  print(total);
+
+  //camelcase = additionResult
+}
