658f464232be06e82f53d7816fde26ee39afda66
shaon <dashaon72@gmail.com>
2024-07-05 21:30:30 +0600

    Practice on dart Set 24

---
+++ bin/set.dart
@@ -0,0 +1,12 @@
+// In Dart language, a set is an unordered collection of unique items.
+void main(){
+Set<int> serialnolist ={4,5,1,43,45,2,52,55,1};  //Set only unic valu contain
+serialnolist.add(35);
+print(serialnolist);
+print(serialnolist.first);
+print(serialnolist.last);
+print(serialnolist.length);
+serialnolist.addAll({300, 100,800,00,334});
+print(serialnolist);
+
+}
