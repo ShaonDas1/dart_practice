0465302f3de1e6a9b334e6f1f7cfcfa00aeef469
shaon <dashaon72@gmail.com>
2024-07-07 01:13:55 +0600

    Practice on dart constant 24

---
+++ bin/constant.dart
@@ -0,0 +1,10 @@
+main(){
+  const String name='joy';  //compile time constant
+  print(name);
+
+  late final double result=3.66 ; //run time constant
+  // result=3.77;
+  // result=3.50;
+  print(result);
+
+}
